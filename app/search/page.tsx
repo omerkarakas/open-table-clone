c3ce552eec7263df94f0f6f2f61665d63e756075
omerkarakas <karakasomer@gmail.com>
2023-10-29 13:53:16 +0300

    Search by location functionality added

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,18 +1,59 @@
+import { PrismaClient, Restaurant } from "@prisma/client";
 import React from "react";
 import Header from "./components/Header";
-import RestaurantCard from "./components/RestaurantCard";
+import RestaurantCard, { RestaurantCardProps } from "./components/RestaurantCard";
 import SearchSideBar from "./components/SearchSideBar";
 
 type Props = {};
 
-const Search = (props: Props) => {
+const prisma = new PrismaClient();
+const select = {
+  id: true,
+  name: true,
+  main_image: true,
+  price: true,
+  cuisine: true,
+  location: true,
+  slug: true,
+};
+const fetchRestaurantsByCity = async (city: string | null) => {
+  if (!city) return prisma.restaurant.findMany({ select });
+
+  return prisma.restaurant.findMany({
+    where: { location: { name: { equals: city.toLowerCase() } } },
+    select,
+  });
+};
+
+const Search = async (props: any) => {
+  // const searchParams = useSearchParams();
+  // const city = searchParams.get("city");
+  // need to change to a client component
+
+  //or directly from the prop
+  const city = props?.searchParams?.city;
+
+  // if (!city) {
+  //   throw new Error("Err");
+  // }
+  const restaurants = await fetchRestaurantsByCity(city);
+  // console.log("restaurants:", { restaurants });
+
   return (
     <>
       <Header />
       <div className="flex py-4 m-auto w-2/3 justify-between items-start">
         <SearchSideBar />
         <div className="w-5/6">
-          <RestaurantCard />
+          {restaurants.length === 0 ? (
+            <h3>No restaurant in this city</h3>
+          ) : (
+            <div>
+              {restaurants.map((restaurant, index) => {
+                return <RestaurantCard restaurant={restaurant} />;
+              })}
+            </div>
+          )}
         </div>
       </div>
     </>
